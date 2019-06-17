cb11c13e8209a44418619dbd0588457063ec68e4
mary.vokicic@vomar.de <mary.vokicic@starfinanz.de>
2019-06-17 21:49:46 +0200

    feat: add request to server

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,75 @@
 import localforage from 'localforage';
 
 var fileInput = document.getElementById('fileInput');
-    var fileDisplayArea = document.getElementById('fileDisplayArea');
-    var downloadButton = document.getElementById('download');
+var fileDisplayArea = document.getElementById('fileDisplayArea');
+var downloadButton = document.getElementById('download');
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+//toDo store image in DB
+var store = localforage.createInstance({
+  name: "nameHere"
+});
+
+var otherStore = localforage.createInstance({
+  name: "images"
+});
+
+fileInput.addEventListener('change', function (e) {
+
+  console.log(fileInput.value);
+
+  var file = fileInput.files[0];
+  var imageType = /image.*/;
+
+  if (file.type.match(imageType)) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      fileDisplayArea.innerHTML = "";
+
+      // Create a new image.
+      var img = new Image();
+      // Set the img src property using the data URL.
+      img.width = 200;
+      img.src = reader.result;
+
+      // Add the image to the page.
+      fileDisplayArea.appendChild(img);
+
+      const renderedHtml = document.getElementById('renderedHtml');
 
 
-    function download(filename, text) {
-      var element = document.createElement('a');
-      element.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(text));
-      element.setAttribute('download', filename);
+      fetch('http://localhost:8080').then(async res => {
 
-      element.style.display = 'none';
-      document.body.appendChild(element);
+        const generatedHtml = await res.text();
+        renderedHtml.contentDocument.body.innerHTML =  generatedHtml;
+        localforage.setItem('image', generatedHtml, (e) => console.log('image stored in indexedDB'));
+      })
+    }
 
-      element.click();
-
-      document.body.removeChild(element);
+    reader.readAsDataURL(file);
+  } else {
+    fileDisplayArea.innerHTML = "File not supported!";
   }
 
-    //toDo store image in DB
-    var store = localforage.createInstance({
-        name: "nameHere"
-    });
 
-    var otherStore = localforage.createInstance({
-        name: "images"
-    });
+});
 
-    fileInput.addEventListener('change', function (e) {
-        console.log(fileInput.value);
+downloadButton.addEventListener('click', function (e) {
 
-        var file = fileInput.files[0];
-        var imageType = /image.*/;
-
-        if (file.type.match(imageType)) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                fileDisplayArea.innerHTML = "";
-
-                // Create a new image.
-                var img = new Image();
-                // Set the img src property using the data URL.
-                img.width = 200;
-                img.src = reader.result;
-
-                // Add the image to the page.
-                fileDisplayArea.appendChild(img);
-
-                const renderedHtml = document.getElementById('renderedHtml');
-
-                const generatedHtml = `<h1>Hello!</h1>
-                <img src="https://via.placeholder.com/150"/>
-                <p>This is a paragraph</p>`
-
-                renderedHtml.contentDocument.body.innerHTML =  generatedHtml
-                localforage.setItem('image', generatedHtml, (e) => console.log('image stored in indexedDB'));
-
-            }
-
-            reader.readAsDataURL(file);
-        } else {
-            fileDisplayArea.innerHTML = "File not supported!";
-        }
-
-
-    });
-
-downloadButton.addEventListener('click', function (e){
-
-  localforage.getItem('image', function(err, value) {
+  localforage.getItem('image', function (err, value) {
     download('wireframe.html', value);
   });
 
